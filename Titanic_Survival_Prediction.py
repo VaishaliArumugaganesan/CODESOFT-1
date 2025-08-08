160889742b1f15e08b8a0543bafd398535f355bd
VaishaliArumugaganesan <vaishali.arumugaganesan@gmail.com>
2025-08-08 20:08:19 +0530

    task -1 Titanic_Survival_Prediction.py

--- Titanic_Survival_Prediction.py
+++ Titanic_Survival_Prediction.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[43]:
-
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -14,21 +8,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 
-# In[33]:
-
-
 df = pd.read_csv("Titanic-Dataset.csv")
 df.head()
 
 
-# In[34]:
-
-
 df.info()
 df.isnull().sum()
-
-
-# In[35]:
 
 
 if 'Cabin' in df.columns:
@@ -49,8 +34,6 @@ if 'Embarked' in df.columns:
     df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
 
 
-# In[36]:
-
 
 if 'Sex' in df.columns:
     df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
@@ -59,10 +42,6 @@ if 'Embarked' in df.columns:
     df = pd.get_dummies(df, columns=['Embarked'], drop_first=True)
 
 df.head()
-
-
-# In[37]:
-
 
 features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']
 if 'Embarked_Q' in df.columns:
@@ -73,32 +52,16 @@ if 'Embarked_S' in df.columns:
 X = df[features]
 y = df['Survived']
 
-
-# In[38]:
-
-
 print("Missing values in X:\n", X.isnull().sum())
-
 
 X = X.dropna()
 y = y.loc[X.index] 
 
 
-# In[39]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-# In[40]:
-
 
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
-
-
-# In[41]:
-
 
 y_pred = model.predict(X_test)
 
@@ -106,19 +69,8 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 print("Classification Report:\n", classification_report(y_test, y_pred))
 
-
-# In[42]:
-
-
 sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, cmap='Blues', fmt='d')
 plt.title("Confusion Matrix")
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.show()
-
-
-# In[ ]:
-
-
-
-
