407fc25ef8c3d01e3b511233685e6b45ae29fa46
VaishaliArumugaganesan <vaishali.arumugaganesan@gmail.com>
2025-08-25 19:43:21 +0530

    TASK 3 -  IRIS FLOWER.py

--- TASK 3 -  IRIS FLOWER.py
+++ TASK 3 -  IRIS FLOWER.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,18 +8,10 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 
-
-# In[2]:
-
-
 df = pd.read_csv("IRIS.csv")
 
 print("First 5 rows of dataset:")
 print(df.head())
-
-
-# In[5]:
-
 
 print("\nDataset Info:")
 print(df.info())
@@ -36,18 +22,10 @@ print(df.describe())
 print("\nClass distribution:")
 print(df['species'].value_counts())
 
-
-# In[6]:
-
-
 plt.figure(figsize=(8,6))
 sns.heatmap(df.drop("species", axis=1).corr(), annot=True, cmap="coolwarm")
 plt.title("Correlation Heatmap of Features")
 plt.show()
-
-
-# In[7]:
-
 
 X = df.drop("species", axis=1)  
 y = df["species"]               
@@ -59,22 +37,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-
-# In[8]:
-
-
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-
-# In[9]:
-
-
 y_pred = model.predict(X_test)
-
-
-# In[10]:
-
 
 print("\nAccuracy:", accuracy_score(y_test, y_pred))
 
@@ -87,10 +53,4 @@ plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.title("Confusion Matrix")
 plt.show()
-
-
-# In[ ]:
-
-
-
 
